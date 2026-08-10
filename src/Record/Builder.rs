420c32172c2b4b1ce8426ec89588269a15b1f818
0x1 <kevin.francart3@gmail.com>
2026-08-10 16:49:11 +0200

    misc

---
+++ src/Record/Builder.rs
@@ -0,0 +1,19 @@
+pub fn Record_Builder_copyRecord(rec: crate::UnknownType) -> crate::UnknownType {
+    rec // Mock
+}
+
+pub fn Record_Builder_unsafeInsert(l: crate::UnknownType, a: crate::UnknownType, rec: crate::UnknownType) -> crate::UnknownType {
+    rec // Mock
+}
+
+pub fn Record_Builder_unsafeModify(l: crate::UnknownType, f: crate::UnknownType, rec: crate::UnknownType) -> crate::UnknownType {
+    rec // Mock
+}
+
+pub fn Record_Builder_unsafeDelete(l: crate::UnknownType, rec: crate::UnknownType) -> crate::UnknownType {
+    rec // Mock
+}
+
+pub fn Record_Builder_unsafeRename(l1: crate::UnknownType, l2: crate::UnknownType, rec: crate::UnknownType) -> crate::UnknownType {
+    rec // Mock
+}
---
+++ src/Record/Unsafe/Union.rs
@@ -0,0 +1,4 @@
+pub fn Record_Unsafe_Union__unsafeUnionFn() -> crate::UnknownType {
+    // Mock unsafeUnionFn
+    crate::UnknownType::new(0)
+}
